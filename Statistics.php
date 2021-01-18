586925760699601a9b88dbe995433c4a975ccddc
David Alienyi <alienyidavid4christ@gmail.com>
2021-01-18 15:40:11 +0100

    dd

--- Statistics.php
+++ Statistics.php
@@ -38,12 +38,12 @@
                     Statistics::HOME => 0,
                     Statistics::AWAY => 0,
                     Statistics::DRAW => 0,
-                    Statistics::HOME_DRAW => 0,
-                    Statistics::AWAY_DRAW => 0,
+                    // Statistics::HOME_DRAW => 0,
+                    // Statistics::AWAY_DRAW => 0,
                     Statistics::OVER_2_POINT_5 => 0,
                     Statistics::UNDER_2_POINT_5 => 0,
                     Statistics::GOAL_GOAL => 0,
-                    Statistics::NO_GOAL_GOAL => 0,
+                    // Statistics::NO_GOAL_GOAL => 0,
                     // Statistics::ANY_BODY_WIN => 0
                 );
 
